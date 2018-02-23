96d1c7c1a460b7c87afd9aa554c350c681eb477b
andrestevens449 <andrestevens449@gmail.com>
2018-02-23 15:58:50 +1100

    Created "GoingHome" game

---
+++ Assets/~GoneHome/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace GoneHome
+{
+    public class Player : MonoBehaviour
+    {
+        public float movementSpeed = 10f;
+
+        private Rigidbody rigid;
+
+        // Use this for initialization
+        void Start()
+        {
+            rigid = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Get input
+            float inputH = Input.GetAxis("Horizontal");
+            float inputV = Input.GetAxis("Vertical");
+
+            // Convert input to Vector3 direction
+            Vector3 inputDir = new Vector3(inputH, 0, inputV);
+
+            // Use direction to apply force... also with speed
+            Vector3 position = transform.position;
+
+            position += inputDir * movementSpeed * Time.deltaTime;
+
+            rigid.MovePosition(position);
+        }
+    }
+}
