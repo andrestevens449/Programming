09f401d5104223aac528c7bd90355bd8da99fed8
andre.stevens3 <andre.stevens3@600900J211DE413>
2018-05-18 16:01:05 +1000

    made AHundredBalls game

---
+++ unity/Assets/~AHundredBalls/Scripts/Bucket.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AHundredBalls
+{
+    public class Bucket : MonoBehaviour
+    {
+        public float speed = 10f;
+        public Vector2 direction = Vector2.right;
+
+        // Use this for initialization
+        void Start()
+        {
+            transform.position += (Vector3)direction * speed * Time.deltaTime;
+        }
+    }
+}
---
+++ unity/Assets/~AHundredBalls/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AHundredBalls
+
+{
+    public class Spawner : MonoBehaviour
+    {
+        public GameObject prefab;
+        public float spawnDelay = 1f;
+
+        // Use this for initialization
+        void Start()
+        {
+            StartCoroutine(Spawn());
+        }
+        
+        IEnumerator Spawn()
+        {
+            yield return new WaitForSeconds(spawnDelay);
+
+            Instantiate(prefab, transform.position, transform.rotation);
+
+            StartCoroutine(Spawn());
+
+        }
+        
+    }
+}
