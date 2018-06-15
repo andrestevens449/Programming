eb126ceff604f0174bdb625aa8a2f6ff60f055ce
andre.stevens3 <andre.stevens3@600900J211DE413>
2018-06-15 13:11:09 +1000

    added my assessment from home into unity

---
+++ unity/Assets/~Assignment/scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+    public float jumpHeight;
+    // storing rigid body in rigid 2D
+    public Rigidbody2D rigid2D;
+    public bool canJump = true;
+    public float movementSpeed;
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Ground")
+        {
+            canJump = true;
+        }
+
+        if (col.gameObject.tag == "Enemy")
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+       rigid2D = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey (KeyCode.Space) && canJump == true)
+        {
+            rigid2D.AddForce(Vector2.up * jumpHeight);
+            canJump = false;
+        }
+        //your able to press the d letter or right arrow to move right
+        if(Input.GetKey (KeyCode.D)|| Input.GetKey (KeyCode.RightArrow))
+        {
+            rigid2D.AddForce(Vector2.right * movementSpeed);
+        }
+	}
+}
---
+++ unity/Assets/~Assignment/scripts/Spawning.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawning : MonoBehaviour 
+{
+    public Vector2 origin;
+    public Vector2 rayDirection = Vector2.down;
+    public float rayDistance = 20f;
+    public GameObject[] walls;
+    public GameObject ground;
+
+    void Start ()
+    {
+        walls = GameObject.FindGameObjectsWithTag("wall");
+        ground = GameObject.FindGameObjectWithTag("Ground");
+	}
+
+    public void FixedUpdate()
+    {
+        origin = transform.position;
+        
+        RaycastHit2D hit = Physics2D.Raycast(origin, rayDirection * rayDistance);
+
+        if (hit.collider == null)
+        {
+            Spawn();
+        }
+        Debug.DrawRay(origin, rayDirection * rayDistance);
+    }
+
+    public void Spawn()
+    {
+        Vector3 spawnPoint = new Vector3(transform.position.x, 0, 0);
+        Instantiate(ground, spawnPoint, Quaternion.identity);
+        for(int i = 0; i < walls.Length; i++)
+        {
+            spawnPoint = new Vector3(spawnPoint.x + 16.5f, 0, 0);
+            Instantiate(walls[Random.Range(0, 2)], spawnPoint, Quaternion.identity);
+        }
+    }
+}
